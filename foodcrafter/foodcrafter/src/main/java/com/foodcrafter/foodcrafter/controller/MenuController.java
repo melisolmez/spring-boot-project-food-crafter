b7c13fb2b9f99ab1800e6e387cec967cf0d0f260
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-03 21:56:40 +0300

    added menu controller

---
+++ foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/controller/MenuController.java
@@ -0,0 +1,32 @@
+package com.foodcrafter.foodcrafter.controller;
+
+import com.foodcrafter.foodcrafter.model.FoodsAndBeverages;
+import com.foodcrafter.foodcrafter.service.FoodsAndBeverageDefaultService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+    private final FoodsAndBeverageDefaultService foodsAndBeverageService;
+    @Autowired
+    public MenuController(FoodsAndBeverageDefaultService foodsAndBeverageService) {
+        this.foodsAndBeverageService = foodsAndBeverageService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FoodsAndBeverages>> foodsAndBeveragesList(){
+        List<FoodsAndBeverages> existLists=foodsAndBeverageService.foodsAndBeveragesList();
+         if(existLists!=null){
+             return new ResponseEntity<>(existLists,HttpStatus.OK);
+         }
+         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
