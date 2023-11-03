4c7123f40e44aba2fecfba5b653c136c4b5fb4dc
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-03 21:56:12 +0300

    added foods and beverage repository

---
+++ foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/repository/FoodsAndBeveragesRepository.java
@@ -0,0 +1,10 @@
+package com.foodcrafter.foodcrafter.repository;
+
+import com.foodcrafter.foodcrafter.model.FoodsAndBeverages;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodsAndBeveragesRepository extends JpaRepository<FoodsAndBeverages, Integer> {
+
+}
