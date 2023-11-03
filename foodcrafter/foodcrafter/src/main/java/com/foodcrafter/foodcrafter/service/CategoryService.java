6bbd14887f58c4df3199a1e696af38f29c74975d
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-03 21:54:58 +0300

    added category service

---
+++ foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/service/CategoryService.java
@@ -0,0 +1,18 @@
+package com.foodcrafter.foodcrafter.service;
+
+import com.foodcrafter.foodcrafter.model.Category;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Validated
+public interface CategoryService {
+
+    Category saveCategory(Category category);
+
+    Category updateCategory(Category category,int id);
+
+    boolean deleteCategory(int id);
+
+    List<Category> listCategory();
+}
