55aff5e40f71b127ded6dcea15e316fb7d395b4d
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-03 21:56:54 +0300

    added project

---
+++ foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/FoodCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.foodcrafter.foodcrafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCrafterApplication.class, args);
+	}
+
+}
---
+++ foodcrafter/foodcrafter/src/test/java/com/foodcrafter/foodcrafter/FoodcrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodcrafter.foodcrafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcrafterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
