cd03fd53444e0565361a61e3d75441a1ab703a9c
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-06 20:44:00 +0300

    added category exception handler

---
+++ foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/response/CategoryExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.foodcrafter.foodcrafter.response;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CategoryExceptionHandler {
+}
