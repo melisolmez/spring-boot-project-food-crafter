d15c58082c721949c31059a33833bf4fc442fa06
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-06 20:45:18 +0300

    update foods and beverage repository

--- foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/repository/FoodsAndBeveragesRepository.java
+++ foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/repository/FoodsAndBeveragesRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FoodsAndBeveragesRepository extends JpaRepository<FoodsAndBeverages, Integer> {
-
+    FoodsAndBeverages findOneFoodsAndBeverageById(int id);
 }
