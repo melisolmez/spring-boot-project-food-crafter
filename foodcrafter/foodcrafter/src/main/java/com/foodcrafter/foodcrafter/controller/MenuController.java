efe1fa2351c35d58205241d3e25cf255b8eb7911
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-06 20:45:28 +0300

    update menu controller

--- foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/controller/MenuController.java
+++ foodcrafter/foodcrafter/src/main/java/com/foodcrafter/foodcrafter/controller/MenuController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,14 @@ public class MenuController {
              return new ResponseEntity<>(existLists,HttpStatus.OK);
          }
          return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FoodsAndBeverages> getOneFoodsAndBeverage(@PathVariable int id){
+        FoodsAndBeverages existFood= foodsAndBeverageService.getFoodsAndBeverageById(id);
+        if(existFood!=null){
+            return new ResponseEntity<>(existFood,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
